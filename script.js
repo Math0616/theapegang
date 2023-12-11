d89d9c9572e6e9ebaa4e6d56140d3b100747d41f
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-11 20:09:36 +0100

    update

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ lazyImages.forEach(img => {
 }
 
 function fetchPriceData(tokenId, galleryItem) {
-    const options = {method: 'GET', headers: {accept: 'application/json', Authorization: Bearer ca4dcecc-5284-49e0-9c1d-0bbfb2001e7e}};
+    const options = {method: 'GET', headers: {accept: 'application/json', Authorization: 'Bearer ca4dcecc-5284-49e0-9c1d-0bbfb2001e7e'}};
     const apiUrl = `https://api-mainnet.magiceden.dev/v2/ord/btc/tokens?tokenIds=${tokenId}&showAll=true&sortBy=priceAsc`;
 
     fetch(apiUrl, options)
