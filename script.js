01f2388527e745592bd04d767b990a1a606ca1cb
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-11 18:06:55 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,100 @@
+document.addEventListener('DOMContentLoaded', function() {
+	fetch('images.json')
+	.then(response => response.json())
+	.then(data => {
+		const gallery = document.querySelector('.gallery');
+		data.forEach(image => {
+		const imageUrl = `https://ord-mirror.magiceden.dev/content/${image.tokenId}`;
+
+		// Create gallery item container
+		const galleryItem = document.createElement('div');
+		galleryItem.classList.add('gallery-item');
+
+		// Setting the 'number' data attribute
+		galleryItem.dataset.number = Array.isArray(image.number) ? image.number.join(', ') : image.number.toString();
+
+		// Set eyeColor and other optional attributes as data attributes
+		const attributes = ['eyeColor'];
+		attributes.forEach(attr => {
+			if (image[attr]) {
+			galleryItem.dataset[attr] = image[attr];
+			}
+		});
+
+		// Create link element
+		const link = document.createElement('a');
+		link.href = `https://magiceden.io/ordinals/item-details/${image.tokenId}`;
+		link.target = "_blank";
+
+		// Create image container
+		const imageContainer = document.createElement('div');
+		imageContainer.classList.add('image-container');
+
+		// Create and set image element
+		const img = document.createElement('img');
+		img.dataset.src = imageUrl;
+		img.alt = `Ordinal Maxi Biz #${image.tokenId}`;
+		img.classList.add('lazyload');
+
+		let hoverTimeout; // Variable to store the hover state timeout
+
+		// Add mouseover event listener with a delay for the tooltip
+		img.addEventListener('mouseover', function(event) {
+			hoverTimeout = setTimeout(function() {
+				showTooltip(event, image);
+			}, 1000); // Delay of 1 second
+		});
+
+		// Add mouseout event listener to hide tooltip and clear the hover timeout
+		img.addEventListener('mouseout', function() {
+			clearTimeout(hoverTimeout);
+			hideTooltip();
+		});
+
+		// Append image to its container
+		imageContainer.appendChild(img);
+
+		// Append image container to link
+		link.appendChild(imageContainer);
+
+		// Append link to gallery item
+		galleryItem.appendChild(link);
+
+		// Price tag with Bitcoin symbol
+		if (image.price) {
+			const priceTag = document.createElement('div');
+			priceTag.classList.add('price-tag');
+			priceTag.textContent = `₿${image.price}`;
+			galleryItem.appendChild(priceTag);
+		}
+
+		// Append gallery item to gallery
+		gallery.appendChild(galleryItem);
+		});
+		
+		initializeLazyLoad(); // After adding all images to the gallery, initialize lazy loading
+	})
+	.catch(error => console.error('Error loading image data:', error));
+});  
+
+function initializeLazyLoad() {
+const lazyImages = document.querySelectorAll('img.lazyload');
+const imageObserver = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+	if (entry.isIntersecting) {
+		const img = entry.target;
+		img.src = img.getAttribute('data-src');
+		img.classList.remove('lazyload');
+		observer.unobserve(entry.target);
+	}
+	});
+});
+
+lazyImages.forEach(img => {
+	imageObserver.observe(img);
+});
+}
+
 function filterSelection(color) {
     var elements = document.getElementsByClassName('gallery-item');
     for (var i = 0; i < elements.length; i++) {
