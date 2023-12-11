47fa4ef972502390ee56a5f208b9ac6eefd49506
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-11 18:17:41 +0100

    update

--- script.js
+++ script.js
@@ -57,6 +57,9 @@ document.addEventListener('DOMContentLoaded', function() {
 		initializeLazyLoad(); // After adding all images to the gallery, initialize lazy loading
 	})
 	.catch(error => console.error('Error loading image data:', error));
+
+    // Initialize filter buttons
+    initializeFilterButtons();
 });  
 
 function initializeLazyLoad() {
@@ -77,62 +80,27 @@ lazyImages.forEach(img => {
 });
 }
 
-function filterSelection(color) {
-    var elements = document.getElementsByClassName('gallery-item');
-    for (var i = 0; i < elements.length; i++) {
-    if (color === 'all' || elements[i].classList.contains(color)) {
-        w3AddClass(elements[i], 'show');
-    } else {
-        w3RemoveClass(elements[i], 'show');
-    }
-    }
-}
+function initializeFilterButtons() {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const filter = this.getAttribute('onclick').match(/'([^']+)'/)[1];
+            filterSelection(filter);
 
-function w3AddClass(element, name) {
-    var arr1 = element.className.split(" ");
-    if (arr1.indexOf(name) === -1) {
-    element.className += " " + name;
-    }
-}
-
-function w3RemoveClass(element, name) {
-    var arr1 = element.className.split(" ");
-    var index = arr1.indexOf(name);
-    if (index > -1) {
-    arr1.splice(index, 1);
-    }
-    element.className = arr1.join(" ");
-}
-
-var btnContainer = document.getElementById("filter-container");
-var btns = btnContainer.getElementsByClassName("filter-btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-    }
-    this.className += " active";
+            // Update active button style
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+        });
     });
 }
 
-document.getElementById('price-filter-checkbox').addEventListener('change', function() {
-    var elements = document.getElementsByClassName('gallery-item');
-    var isChecked = this.checked;
-
-    for (var i = 0; i < elements.length; i++) {
-    // Check if the item has a price tag
-    var hasPriceTag = elements[i].getElementsByClassName('price-tag').length > 0;
-
-    if (isChecked && hasPriceTag) {
-        elements[i].style.display = 'block';
-    } else if (isChecked && !hasPriceTag) {
-        elements[i].style.display = 'none';
-    } else {
-        elements[i].style.display = ''; // Or use 'block' to show all
-    }
-    }
-});
-
-// Initially display all items
-filterSelection('all');
+function filterSelection(filter) {
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    galleryItems.forEach(item => {
+        if (filter === 'all' || item.dataset.eyeColor === filter) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
